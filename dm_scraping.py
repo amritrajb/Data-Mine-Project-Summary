2afb98e7bc439268fc23f932d1fd81bc2932b4cd
amritrajb <86334622+amritrajb@users.noreply.github.com>
2022-08-15 23:29:08 -0700

    added excel support

--- dm_scraping.py
+++ dm_scraping.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import xlsxwriter
+import pandas as pd
 
 url = "https://projects.the-examples-book.com/companies/"
 reqs = requests.get(url)
@@ -11,6 +13,7 @@ def extract_project_data(url):
     soup = BeautifulSoup(reqs.text, 'html.parser')
     text_content = soup.get_text()
     lines = text_content.split("\n")
+    project_info_dict["url"] = url
     for index, element in enumerate(lines):
         if "Lecture time:" in element:
             lecture_time = element.split(":",1)
@@ -39,14 +42,14 @@ def extract_project_data(url):
     return project_info_dict
 
 print(extract_project_data("https://projects.the-examples-book.com/projects/protect-children-from-accidental-exposure/"))
-exit(1)
- 
+
 url_dict = {}
 for link in soup.find_all('a'):
     url_name = "https://projects.the-examples-book.com" + link.get('href')
     #print(url_name)
     url_dict[url_name] = []
 
+project_list = []
 for url_name in url_dict:
     reqs = requests.get(url_name)
     soup = BeautifulSoup(reqs.text, 'html.parser')
@@ -55,6 +58,19 @@ for url_name in url_dict:
         sub_url_name = "https://projects.the-examples-book.com" + sub_url.get('href')
         url_dict[url_name].append(sub_url_name)
 
-print(url_dict)
+
+
+# Create a Pandas dataframe from some data.
+df = pd.DataFrame.from_dict(url_dict, orient='index')
+#print(df.head)
+
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter('data_mine_projects.xlsx', engine='xlsxwriter')
+
+# Convert the dataframe to an XlsxWriter Excel object.
+df.to_excel(writer, sheet_name='2022-2023 Projects')
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
 
     
